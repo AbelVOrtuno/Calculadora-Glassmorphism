28cecd15f7ae5f675a4ac20c4491b16e6d6c1f91
Abel <152920117+AbelVOrtuno@users.noreply.github.com.>
2026-03-09 11:29:42 -0600

    "Evitamos duplicar operadores"

--- js/script.js
+++ js/script.js
@@ -2,6 +2,14 @@
 let display = document.getElementById("display")
 
 function addToDisplay(value){
+
+    let lastChar = display.innerText.slice(-1)
+    let operators = ["+","-","*","/"]
+
+    if(operators.includes(lastChar) && operators.includes(value)){
+        return
+    }
+
     if(display.innerText === "0"){
         display.innerText = value
     }else{
@@ -19,10 +27,11 @@ function calculate(){
         display.innerText = "Error"
     }
 }
+/* Boton DEL o borrar */
 function deleteLast(){
     display.innerText = display.innerText.slice(0,-1)
 
     if(display.innerText === ""){
-        display.innerText =""
+        display.innerText ="0"
     }
 }
